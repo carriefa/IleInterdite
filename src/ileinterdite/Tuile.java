72ad5730d0e3ec317b9b47dfd32567e3c7150903
carriefa <carriefa@pc-dg-037-13>
2017-05-29 11:35:33 +0200

    Merge origin/master
    Conflicts:
    	src/ileinterdite/Controleur.java

--- src/ileinterdite/Tuile.java
+++ src/ileinterdite/Tuile.java
@@ -1,15 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ileinterdite;
 
-/**
- *
- * @author carriefa
- */
-public class Tuile {
+import java.util.ArrayList;
 
+public class Tuile {
+	private ArrayList<Aventurier__Abs_> _pionPrésents = new ArrayList<Aventurier__Abs_>();
+	public Carte_Inondation _carte;
+	public Etat _etat;
+	private Trésor _tresorAssocié;
+        private int numero;
+        private String nom;
     
+        public Tuile(int numero,String nom){
+            setNumero(numero);
+            setNom(nom);
+
+        
+        }
+
+        public void setNumero(int numero) {
+            this.numero=numero;
+        }
+
+        public void setNom(String nom) {
+            this.nom=nom;
+        }
+        
+	public void MiseAJourDeplacement(Tuile aDepart, Tuile aArrivee, Aventurier__Abs_ aJoueur) {
+		throw new UnsupportedOperationException();
+	}
+
+	public Etat GetEtat() {
+		throw new UnsupportedOperationException();
+	}
 }
