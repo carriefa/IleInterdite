4bc73a68209441388c22a93b736effb2f635e820
carriefa <carriefa@pc-dg-037-13>
2017-05-29 11:52:11 +0200

    Merge origin/master
    Conflicts:
    	src/ileinterdite/Controleur.java

--- src/ileinterdite/Aventurier.java
+++ src/ileinterdite/Aventurier.java
@@ -2,12 +2,14 @@ package ileinterdite;
 
 import java.util.ArrayList;
 
-public abstract class Aventurier__Abs_ {
+public abstract class Aventurier {
 	public ArrayList<CarteTrésor_Abs_> _main = new ArrayList<CarteTrésor_Abs_>();
 	private Joueur _joueurassocié;
 	private Tuile _positionActuelle;
 
-	public Role getRole() {
+	public Aventurier getRole() {
 		throw new UnsupportedOperationException();
 	}
+        
+        
 }
