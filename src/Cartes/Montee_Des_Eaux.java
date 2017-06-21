bdd93e7233aa543dc48668c94e2c67e2996a7d14
graff <graff@DESKTOP-4BFAC38.home>
2017-06-21 11:15:45 +0200

    Merge origin/master
    Conflicts:
    	src/ileinterdite/JeuIleInterdite.java
    	src/ileinterdite/Joueur.java
    	src/ileinterdite/Montee_Des_Eaux.java

--- src/Cartes/Montee_Des_Eaux.java
+++ src/Cartes/Montee_Des_Eaux.java
@@ -3,4 +3,9 @@ package Cartes;
 import Cartes.Carte_Tresor_Abs;
 
 public class Montee_Des_Eaux extends Carte_Tresor_Abs {
+private String type = "Montée des eaux";
+    @Override
+    public String getType() {
+        return type;
+    }
 }
